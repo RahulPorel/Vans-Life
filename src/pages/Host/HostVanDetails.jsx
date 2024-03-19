8fdef440e19fbcbf37e238dce150b1290c91fac1
rahulporel51 <rahulporel51@gmail.com>
2024-03-19 21:04:52 +0530

    code update

--- src/pages/Host/HostVanDetails.jsx
+++ src/pages/Host/HostVanDetails.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { json, useParams } from "react-router-dom";
 
 const HostVanDetails = () => {
+  const { id } = useParams();
+  const [currentVan, setCurrentVan] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCurrentVan(data.vans));
+  }, []);
+
   return <div>HostVanDetails</div>;
 };
 
