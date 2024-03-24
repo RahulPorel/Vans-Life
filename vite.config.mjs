d18aa0ec84383e0b60b3ad2c902a2b76f0fc7a2e
rahulporel51 <rahulporel51@gmail.com>
2024-03-24 11:15:04 +0530

    config file up

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/VanLife/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
