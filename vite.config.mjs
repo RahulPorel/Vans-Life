9923d7dd3d4ac0d9088a3a173ed2af6d83276bf4
rahulporel51 <rahulporel51@gmail.com>
2024-03-24 11:12:44 +0530

    readme update

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/VanLife/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "~": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
+    },
+  },
 });
