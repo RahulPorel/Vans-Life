d60331e9416054882593f418a4cad2d9c8db175a
rahulporel51 <rahulporel51@gmail.com>
2024-03-24 10:54:53 +0530

    update config files

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	base: "/VanLife/",
-	plugins: [react()]	
-})
+  base: "/VanLife/",
+  plugins: [react()],
+});
