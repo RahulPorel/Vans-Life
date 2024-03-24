422dc23e36b2a3d384a48b469f607c0f0581789b
rahulporel51 <rahulporel51@gmail.com>
2024-03-24 12:31:10 +0530

    commit

--- src/components/HostLayout/HostLayout.jsx
+++ src/components/HostLayout/HostLayout.jsx
@@ -1,9 +1,7 @@
-
 import { NavLink, Outlet } from "react-router-dom";
 import "./HostLayout.css";
 
 const HostLayout = () => {
-  
   const activeStyle = {
     fontWeight: "bold",
     textDecoration: "underline",
