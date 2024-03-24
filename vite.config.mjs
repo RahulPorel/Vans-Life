0ae4153edac95421866a41422793917678810a7d
rahulporel51 <rahulporel51@gmail.com>
2024-03-24 11:20:20 +0530

    config ch

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/vans",
   plugins: [react()],
   resolve: {
     alias: {
